780cf91d5e20eafd73df07c4c527c82933e359c8
sanu0077 <sunayana4112@gmail.com>
2024-08-09 20:30:47 +0530

    9th question done

---
+++ questions/9th_question.py
@@ -0,0 +1,10 @@
+# Q9. Leap Year Checker
+# Problem: Determine if a year is a leap year.
+# (Leap years are divisible by 4, but not by 100 unless also divisible by 400).
+
+year = int(input("Enter the year :"))
+
+if (year % 400 == 0) or (year % 4 ==0 and year %100 !=0) :
+    print("It is a leap year")
+else :
+    print("It is not leapear")    
