3099e892083665d5a27e323d3619633b21a5d5e3
sanu0077 <sunayana4112@gmail.com>
2024-08-19 01:46:14 +0530

    3rd done

---
+++ function/3rd_question.py
@@ -0,0 +1,9 @@
+# Q3. Polymorphism in Functions
+# Problem: Write a function multiply that multiplies two numbers,
+# but can also accept and multiply strings.
+
+def multiply(num1 , num2):
+    return num1 * num2
+
+result = multiply(7,"kaku")
+print(result)
---
+++ function/4th_question.py
@@ -0,0 +1,5 @@
+# Q4. Function Returning Multiple Values
+# Problem: Create a function that returns both the
+# area and circumference of a circle given its radius.
+
+
