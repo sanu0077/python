3b410b57a5c5f706ac71a2ded7aa2e186629bfce
sanu0077 <sunayana4112@gmail.com>
2024-08-19 02:04:51 +0530

    4th done

--- function/4th_question.py
+++ function/4th_question.py
@@ -3,3 +3,26 @@
 # area and circumference of a circle given its radius.
 
 
+# import math
+
+# def area(radius):
+#     return math.pi * (radius ** 2)
+# def circumferenece(radius):
+#     return 2 * math.pi * radius
+
+# print(area(9),circumferenece(9))
+
+
+
+import math
+
+def circle_stat(radius):
+    area = math.pi * radius ** 2
+    circumference = 2 * math.pi * radius
+    return area, circumference
+
+area, circumference = circle_stat(9)
+
+# Print the values with 2 decimal places
+print(f"Area: {area:.2f}, Circumference: {circumference:.2f}")
+
