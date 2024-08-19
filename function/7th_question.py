38318dace49d45dafa2a86e45d7b0e37d2bf2b0a
sanu0077 <sunayana4112@gmail.com>
2024-08-19 11:58:25 +0530

    7th done

---
+++ function/7th_question.py
@@ -0,0 +1,16 @@
+# Q7. Function with *args
+# Problem: Write a function that takes variable number 
+# of arguments and returns their sum.
+
+
+def sum_all(*args):
+    return sum(args)
+
+print(sum_all(1,2,3,4,5,6,7,8))
+
+
+
+# def sum_all(*args):
+#     return sum(args)
+
+# print(sum_all(1, 2, 3, 4, 5, 6, 7, 8))
